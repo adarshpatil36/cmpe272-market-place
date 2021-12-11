b5caa5ab333ac43e8494cca4a2e145d0909ec23f
adpatil036 <adarsh.patil@sjsu.edu>
2021-12-11 15:35:45 -0800

    added reviews tab

---
+++ reviews.php
@@ -0,0 +1,79 @@
+<link rel="stylesheet" href="reviews.css">
+
+<div class="review-container">
+    <h2>Reviews</h2>
+    <label for="search" class="input-field">
+        <i class="fas fa-search"></i>
+        <input id="search" placeholder="Search for reviews ..." type="text">
+    </label>
+    <div class="suggest">
+        <span>expriance</span>
+        <span>quality</span>
+        <span>design</span>
+        <span>size</span>
+        <span>features</span>
+        <span>values</span>
+        <span>replacement</span>
+    </div>
+
+    <div class="cards">
+        <div class="card">
+            <div class="card-top">
+                <div class="name">
+                    <div class="img one" alt="">NF</div>
+                    <p>Full Name</p>
+                </div>
+                <div class="rate">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star-half"></i>
+                </div>
+            </div>
+
+            <div class="card-content">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem consequuntur, impedit ullam provident
+                    deserunt repellendus fuga totam exercitationem minima molestias?</p>
+            </div>
+
+            <div class="card-action">
+                <span>13 Feb, 2021</span>
+                <button class="btn">
+                    <i class="fas fa-external-link-alt"></i>
+                    Share
+                </button>
+            </div>
+
+        </div>
+        <div class="card">
+            <div class="card-top">
+                <div class="name">
+                    <div class="img">FN</div>
+                    <p>Full Name</p>
+                </div>
+                <div class="rate">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star-half"></i>
+                </div>
+            </div>
+
+            <div class="card-content">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem consequuntur, impedit ullam provident
+                    deserunt repellendus fuga totam exercitationem minima molestias?</p>
+            </div>
+
+            <div class="card-action">
+                <span>13 Feb, 2021</span>
+                <button class="btn">
+                    <i class="fas fa-external-link-alt"></i>
+                    Share
+                </button>
+            </div>
+
+        </div>
+    </div>
+</div>
